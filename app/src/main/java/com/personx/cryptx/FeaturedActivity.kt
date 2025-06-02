f3531d68be16d992dea61b740619adb589cc4bed
thelonewolf06 <thelonewolf06@proton.me>
2025-06-02 20:55:04 +0800

    refactor: update compose BOM version and clean up unused imports in FeaturedActivity

--- app/src/main/java/com/personx/cryptx/FeaturedActivity.kt
+++ app/src/main/java/com/personx/cryptx/FeaturedActivity.kt
@@ -20,7 +20,6 @@ import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -28,20 +27,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.personx.cryptx.components.CyberpunkNavBar
 import com.personx.cryptx.components.Header
 import com.personx.cryptx.data.NavBarItem
-import com.personx.cryptx.screens.DecryptionScreen
-import com.personx.cryptx.screens.EncryptScreen
-import com.personx.cryptx.screens.HashDetector
-import com.personx.cryptx.screens.HashGeneratorScreen
 import com.personx.cryptx.screens.HomeScreen
-import com.personx.cryptx.screens.SteganographyScreen
 import com.personx.cryptx.ui.theme.CryptXTheme
 
 class FeaturedActivity : ComponentActivity() {
