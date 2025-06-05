a1e63a4137997b073e47332711d19db894cd301b
thelonewolf06 <thelonewolf06@proton.me>
2025-06-05 14:14:01 +0800

    refactor: enhance EncryptionHistoryDao with update and delete methods for improved data management

--- app/src/main/java/com/personx/cryptx/database/encryption/EncryptionHistoryDao.kt
+++ app/src/main/java/com/personx/cryptx/database/encryption/EncryptionHistoryDao.kt
@@ -1,14 +1,17 @@
 package com.personx.cryptx.database.encryption
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 
 @Dao
 interface EncryptionHistoryDao {
+
     /**
      * Inserts a new encryption history record into the database.
      * If a record with the same primary key already exists, it will be replaced.
@@ -17,6 +20,13 @@ interface EncryptionHistoryDao {
      */
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
+
+    /* Inserts a new encryption history record into the database.
+     * If a record with the same primary key already exists, it will be replaced.
+     *
+     * @param history The EncryptionHistory object to be inserted.
+     */
+
     suspend fun insertEncryptionHistory(history: EncryptionHistory)
 
     /**
@@ -24,12 +34,37 @@ interface EncryptionHistoryDao {
      *
      * @return A Flow that emits a list of EncryptionHistory objects.
      */
+
     @Query("SELECT * FROM encryption_history ORDER BY timestamp DESC")
     fun getAllEncryptionHistory(): Flow<List<EncryptionHistory>>
+
+    /**
+     * Retrieves the count of all encryption history records in the database.
+     *
+     * @return The count of encryption history records.
+     */
 
     @Query("SELECT COUNT(*) FROM encryption_history")
     suspend fun getCount(): Int
 
+    /**
+     * Updates an existing encryption history record in the database.
+     * The record must already exist; otherwise, it will not be inserted.
+     *
+     * @param history The EncryptionHistory object to be updated.
+     */
+
+    @Update
+    suspend fun updateEncryptionHistory(history: EncryptionHistory)
+
+    /**
+     * Deletes a specific encryption history record from the database.
+     *
+     * @param history The EncryptionHistory object to be deleted.
+     */
+
+    @Delete
+    suspend fun deleteEncryptionHistory(history: EncryptionHistory)
 
     /**
      * Inserts a new decryption history record into the database.
@@ -37,6 +72,7 @@ interface EncryptionHistoryDao {
      *
      * @param history The DecryptionHistory object to be inserted.
      */
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertDecryptionHistory(history: DecryptionHistory)
 
@@ -45,8 +81,27 @@ interface EncryptionHistoryDao {
      *
      * @return A Flow that emits a list of DecryptionHistory objects.
      */
+
     @Query("SELECT * FROM decryption_history ORDER BY timestamp DESC")
     fun getAllDecryptionHistory(): Flow<List<DecryptionHistory>>
+
+    @Update
+    /**
+     * Updates an existing decryption history record in the database.
+     * The record must already exist; otherwise, it will not be inserted.
+     *
+     * @param history The DecryptionHistory object to be updated.
+     */
+    suspend fun updateDecryptionHistory(history: DecryptionHistory)
+
+    /**
+     * Deletes a specific decryption history record from the database.
+     *
+     * @param history The DecryptionHistory object to be deleted.
+     */
+
+    @Delete
+    suspend fun deleteDecryptionHistory(history: DecryptionHistory)
 
 
 }
