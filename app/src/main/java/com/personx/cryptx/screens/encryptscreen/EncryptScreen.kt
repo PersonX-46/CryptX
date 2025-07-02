2723ad0a3dba3dbdb85b55c32bfc866fb33a6af8
thelonewolf <thelonewolf06@proton.me>
2025-07-02 20:05:17 +0800

    refactor: update .gitignore and clean up imports in EncryptScreen and NavGraph

--- app/src/main/java/com/personx/cryptx/screens/encryptscreen/EncryptScreen.kt
+++ app/src/main/java/com/personx/cryptx/screens/encryptscreen/EncryptScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
