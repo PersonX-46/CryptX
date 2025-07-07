acc5162030a0da361ff4020bdf7ce9ebf564e4c7
thelonewolf <thelonewolf06@proton.me>
2025-07-07 20:54:26 +0800

    feat: add constants for secure preferences and application settings

---
+++ app/src/main/java/com/personx/cryptx/Constants.kt
@@ -0,0 +1,13 @@
+package com.personx.cryptx
+
+object SecurePrefs {
+    const val NAME = "secure_prefs"
+    const val SALT = "salt"
+    const val IV = "iv"
+    const val ENCRYPTED_SESSION_KEY = "encryptedSessionKey"
+}
+
+object AppSettingsPrefs {
+    const val NAME = "cryptx_prefs"
+    const val BASE64_DEFAULT = "base64_default"
+}
