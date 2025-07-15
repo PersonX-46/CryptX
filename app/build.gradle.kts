aeeb8ebe038edad61d47fa10a917a1d3ff5daf28
thelonewolf <thelonewolf06@proton.me>
2025-07-15 11:00:31 +0800

    refactor: update version code and name to 1.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.3.1"
+        versionCode = 8
+        versionName = "1.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
