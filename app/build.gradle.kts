e68738d9263b1b7b675d29f95c5daece1f6a7029
thelonewolf <thelonewolf06@proton.me>
2025-08-30 13:48:01 +0800

    refactor: update versionCode and versionName to 9 and 1.4.0 respectively

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.3.2"
+        versionCode = 9
+        versionName = "1.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
